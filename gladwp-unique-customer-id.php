ac1550a657b3affd6cfeab542cc7bc2b2f0f5a13
user <user@example.com>
2022-09-11 16:12:35 -0500

    init

---
+++ gladwp-unique-customer-id.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Gladwp Unique Customer Id
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     gladwp-unique-customer-id
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gladwp_Unique_Customer_Id
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Gladwp_Unique_Customer_Id
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
